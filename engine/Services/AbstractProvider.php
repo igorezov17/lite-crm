a398b0ec40c4edb151a0de2d661482a9546f63fc
Puffic Man <rezaevigor@yandex.ru>
2025-08-26 12:23:49 +0300

    Initial service provider

---
+++ engine/Services/AbstractProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Engine\Services;
+
+abstract class AbstractProvider
+{
+    public function __construct(
+        protected $di,
+    )
+    {}
+
+    abstract function init();
+}
